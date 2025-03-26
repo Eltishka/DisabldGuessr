42f96d7255d10fd190633e38f17ec8b7e86c6ad8
Vova <vladimir@bagmanov.ru>
2025-03-26 21:55:18 +0300

    added file for all currently found points

--- prototype1/test.js
+++ prototype1/test.js
@@ -131,8 +131,8 @@ function showDistance() {
 
 let startLati = -89;
 let endLati = 89;
-let startLong = 72;
-let endLong = 73;
+let startLong = 74;
+let endLong = 75;
 // let startLati = -89;
 // let endLati = -88;
 // let startLong = 72;
